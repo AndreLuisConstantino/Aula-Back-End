24e8d190e3c3e82d25fbad45ee47e5cb3f63ed73
André <andreluis081205@gmail.com>
2023-02-06 16:53:11 -0300

    Tabuada com loops de repetição

--- Aula 05/modulo/tabuada.js
+++ Aula 05/modulo/tabuada.js
@@ -4,3 +4,39 @@
  * Autor: André
  * Versão: 1.0
  **************************************************************************************/
+
+//Função que gera uma tabuada até uma valor específico
+const getTabuada = function (multiplicando, maxMultiplicador) {
+    let tabuada = Number(String(multiplicando).replace(',', '.'))
+    let tabuadaContador = Number(String(maxMultiplicador).replace(',', '.'))
+    let status = true
+    let resultado
+    let contador = 0 //Start da repetição
+
+    if (tabuada == 0 || tabuadaContador == 0) {
+        status = false
+    } else if (isNaN(tabuada) || isNaN(tabuadaContador)) {
+        status = false
+    } else {
+
+        // while (contador <= tabuadaContador) {
+        //     //2x0 = 0
+        //     resultado = tabuada * contador
+        //     console.log(tabuada + 'x' + contador + '=' + resultado)
+
+        //      contador +=1
+        // }
+
+        for (let contador = 0; contador <= tabuadaContador; contador++) {
+            resultado = tabuada * contador
+            console.log(`${tabuada} x ${contador} = ${resultado}`)
+        }
+    }
+    return status
+}
+
+getTabuada(7, 18)
+
+module.exports = {
+    getTabuada
+}
