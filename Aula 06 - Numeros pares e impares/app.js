358d731031996d7f3627d2e3f048b90ce82c77b4
André Luiz Constantino <andreluis081205@gmail.com>
2023-02-15 20:55:38 -0300

    Projeto pares e impares finalizado

--- Aula 06 - Numeros pares e impares/app.js
+++ Aula 06 - Numeros pares e impares/app.js
@@ -2,6 +2,73 @@
  * Objetivo: App para a separação de numeros pares e impares
  * Data: 06/02/2023
  * Autor: André
- * Versão: 1.0
+ * Versão: 1.1
  **************************************************************************************/
 
+
+//Import da biblioteca da calculadora
+var matematica = require('./modulo/matematica.js')
+
+//import da biblioteca readline
+var readline = require('readline')
+const { close } = require('fs')
+
+//Cria o objeto para ser especialista em entrada de dados pelo teclado
+var entradaDados = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+console.log('***Olá, bem vindo a Calculadora de números pares e impares!***')
+entradaDados.question('Digite o número inicial: ', function(numero1){
+    let numeroInicial = numero1
+
+    entradaDados.question('Digite o número final: ', function(numero2){
+        let numeroFinal = numero2
+
+        entradaDados.question('Você quer ver apenas pares ou impares? \n Digite 1- Pares \n Digite 2- Impares \n Digite 3- Para as duas \n', function(escolhaDigitada){
+            let escolha = escolhaDigitada
+
+            if (escolha == ''){
+                console.log('Você deixou a escolha vazia.')
+                entradaDados.close()
+            } else if (isNaN(escolha)){
+                console.log('O valor digitado na escolha não é um número.')
+                entradaDados.close()
+            } else if(escolha != '1' && escolha != '2' && escolha != '3'){
+                console.log('Digite uma opção válida.')
+                entradaDados.close()
+            } else if(escolha == '1'){
+                let resultadoPares = matematica.separarParesDosImpares(numeroInicial, numeroFinal)
+
+                if(resultadoPares === false){
+                    entradaDados.close()
+                    console.log('Não foi possivel executar o seu programa.')
+                } else {
+                    entradaDados.close()
+                }
+                
+            } else if(escolha == '2'){
+                let resultadoImpares = matematica.separarImparesDosPares(numeroInicial, numeroFinal)
+
+                if(resultadoImpares === false){
+                    entradaDados.close()
+                    console.log('Não foi possivel executar o seu programa.')
+                } else {
+                    entradaDados.close()
+                }
+
+            } else {
+                let resultadoParesImpares = matematica.separar(numeroInicial, numeroFinal)
+         
+                if(resultadoParesImpares === false){
+                    entradaDados.close()
+                    console.log('Não foi possivel executar o seu programa.')
+                } else {
+                    entradaDados.close()
+                }
+            }
+            
+        })
+    })
+})
--- Aula 06 - Numeros pares e impares/modulo/matematica.js
+++ Aula 06 - Numeros pares e impares/modulo/matematica.js
@@ -2,7 +2,7 @@
  * Objetivo: App para fazer o calculo de separação de nuemros pares e impares
  * Data: 06/02/2023
  * Autor: André
- * Versão: 1.0
+ * Versão: 1.1
  **************************************************************************************/
 
 const separarParesDosImpares = function (numeroInicialDigitado, numeroFinalDigitado) {
@@ -11,7 +11,7 @@ const separarParesDosImpares = function (numeroInicialDigitado, numeroFinalDigit
     let status = true
 
     if (numeroInicial == '' || numeroFinal == '') {
-        console.log('Você deixou de entrar algum valor.')
+        console.log('Você deixou de entrar algum valor ou está usando zero.')
         status = false
     } else if (numeroInicial < 0 || numeroInicial > 500) {
         console.log('O número inicial só pode estar entre 0 e 500.')
@@ -24,16 +24,19 @@ const separarParesDosImpares = function (numeroInicialDigitado, numeroFinalDigit
         status = false
     } else {
         console.log('***Tabela de números pares:***')
-        for(numeroInicial; numeroInicial <= numeroFinal; numeroInicial++) {
+        let contagem = 0
+        for (numeroInicial; numeroInicial <= numeroFinal; numeroInicial++) {
             if (numeroInicial % 2 == false) {
                 console.log(numeroInicial)
             }
+            contagem++
         }
+        console.log('A quantidade de números encontrados: ' + contagem)
         return status
     }
 }
 
-const separarImparesDosPares = function (numeroInicialDigitado, numeroFinalDigitado){
+const separar = function (numeroInicialDigitado, numeroFinalDigitado) {
     let numeroInicial = Number(String(numeroInicialDigitado).replace(',', '.'))
     let numeroFinal = Number(String(numeroFinalDigitado).replace(',', '.'))
     let status = true
@@ -41,9 +44,9 @@ const separarImparesDosPares = function (numeroInicialDigitado, numeroFinalDigit
     if (numeroInicial == '' || numeroFinal == '') {
         console.log('Você deixou de entrar algum valor.')
         status = false
-    } else if (isNaN(numeroInicial) || isNaN(numeroFinal)){
+    } else if (isNaN(numeroInicial) || isNaN(numeroFinal)) {
         console.log('Algum dos valores digitados não é um número.')
-    } 
+    }
     else if (numeroInicial < 0 || numeroInicial > 500) {
         console.log('O número inicial só pode estar entre 0 e 500.')
         status = false
@@ -54,20 +57,91 @@ const separarImparesDosPares = function (numeroInicialDigitado, numeroFinalDigit
         console.log('O número inicial não pode ser maior que o final ou igual ao mesmo.')
         status = false
     } else {
-        console.log('***Tabela de números pares:***')
-        for(numeroInicial; numeroInicial <= numeroFinal; numeroInicial++) {
-            if (numeroInicial % 2 == true) {
-                console.log(numeroInicial)
-            }
-        }
+
+        separarParesDosImparesMatematica(numeroInicial, numeroFinal)
+        separarImparesDosParesMatematica(numeroInicial, numeroFinal)
+
+        // console.log('***Tabela de números pares:***')
+        // for(numeroInicial; numeroInicial <= numeroFinal; numeroInicial++) {
+        //     if (numeroInicial % 2 == true) {
+        //         console.log(numeroInicial)
+        //     }
+        // }
         return status
     }
 }
 
-separarParesDosImpares(10, 50)
-separarImparesDosPares(10,70)  
+const separarImparesDosPares = function (numeroInicialDigitado, numeroFinalDigitado) {
+    let numeroInicial = Number(String(numeroInicialDigitado).replace(',', '.'))
+    let numeroFinal = Number(String(numeroFinalDigitado).replace(',', '.'))
+    let status = true
+
+    if (numeroInicial == '' || numeroFinal == '') {
+        console.log('Você deixou de entrar algum valor ou está usando zero.')
+        status = false
+    } else if (numeroInicial < 0 || numeroInicial > 500) {
+        console.log('O número inicial só pode estar entre 0 e 500.')
+        status = false
+    } else if (numeroFinal < 500 || numeroFinal > 1000) {
+        console.log('O número final só pode estar entre 100 e 500.')
+        status = false
+    } else if (numeroInicial >= numeroFinal) {
+        console.log('O número inicial não pode ser maior que o final ou igual ao mesmo.')
+        status = false
+    } else {
+        console.log('***Tabela de números Impares:***')
+        let contagem = 0
+        for (numeroInicial; numeroInicial <= numeroFinal; numeroInicial++) {
+            if (numeroInicial % 2 == true) {
+                console.log(numeroInicial)
+
+            }
+            contagem++
+        }
+        console.log('A quantidade de números encontrados: ' + contagem)
+        return status
+    }
+}
+
+const separarImparesDosParesMatematica = function (numeroInicialDigitado, numeroFinalDigitado) {
+    let numeroInicial = numeroInicialDigitado
+    let numeroFinal = numeroFinalDigitado
+    let status = true
+
+    console.log('***Tabela de números Impares:***')
+    let contagem = 0
+    for (numeroInicial; numeroInicial <= numeroFinal; numeroInicial++) {
+        if (numeroInicial % 2 == true) {
+            console.log(numeroInicial)
+
+        }
+        contagem++
+    }
+    console.log('A quantidade de números encontrados: ' + contagem)
+    return status
+}
+
+const separarParesDosImparesMatematica = function (numeroInicialDigitado, numeroFinalDigitado) {
+    let numeroInicial = numeroInicialDigitado
+    let numeroFinal = numeroFinalDigitado
+    let status = true
+
+    console.log('***Tabela de números pares:***')
+    let contagem = 0
+    for (numeroInicial; numeroInicial <= numeroFinal; numeroInicial++) {
+        if (numeroInicial % 2 == false) {
+            console.log(numeroInicial)
+        }
+        contagem++
+    }
+    console.log('A quantidade de números encontrados: ' + contagem)
+    return status
+}
+
+
 
 module.exports = {
+    separar,
     separarImparesDosPares,
     separarParesDosImpares
 }
