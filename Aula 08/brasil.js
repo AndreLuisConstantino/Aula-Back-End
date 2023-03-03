29b2ca6c12496d7d81493b74714cfd2a25986257
André <andreluis081205@gmail.com>
2023-03-03 16:57:35 -0300

    getCapitalEstado feito

--- Aula 08/brasil.js
+++ Aula 08/brasil.js
@@ -9,36 +9,64 @@
 
 var brasil = require('./estados_cidades')
 
-const getListaDeEstados = function(){
-   let jsonListaDeEstados = {}
-   let uf = []
+const getListaDeEstados = function () {
+    let jsonListaDeEstados = {}
+    let uf = []
 
-   brasil.estadosCidades.estados.forEach(function(estado){
-      uf.push(estado.sigla)
-   })
+    brasil.estadosCidades.estados.forEach(function (estado) {
+        uf.push(estado.sigla)
+    })
 
-   jsonListaDeEstados = {
-      uf,
-      quantidade: brasil.estadosCidades.estados.length
-   }
-   
-   return jsonListaDeEstados
+    jsonListaDeEstados = {
+        uf,
+        quantidade: brasil.estadosCidades.estados.length
+    }
+
+    return jsonListaDeEstados
 }
 
-const getDadosEstado = function(sigla){
-    jsonListasDeDadosEstado = {}
+const getDadosEstado = function (sigla) {
+    let jsonListaDeDadosEstado = {}
+    let status = false
 
-    brasil.estadosCidades.estados.forEach(function(estado){
-        if(estado.sigla == sigla){
-            jsonListasDeDadosEstado = {
+
+    brasil.estadosCidades.estados.forEach(function (estado) {
+        if (estado.sigla == sigla) {
+            jsonListaDeDadosEstado = {
                 uf: estado.sigla,
                 descricao: estado.nome,
                 capital: estado.capital,
                 regiao: estado.regiao
             }
+            status = true
         }
     })
-    return jsonListasDeDadosEstado
+
+    if (status == true)
+        return jsonListaDeDadosEstado
+    else
+        return false
 }
 
-console.log(getDadosEstado('SP'))
+const getCapitalEstado = function (sigla) {
+    let jsonListaCapitalDoEstado = {}
+    let status = false
+
+    brasil.estadosCidades.estados.forEach(function (estado) {
+        if (estado.sigla == sigla){
+            jsonListaCapitalDoEstado = {
+                uf: estado.sigla,
+                descricao: estado.nome,
+                capital: estado.capital,
+            }
+            status = true
+        }
+    })
+
+    if (status == true)
+        return jsonListaCapitalDoEstado
+    else
+        return false
+}
+
+console.log(getCapitalEstado('Sj'))
